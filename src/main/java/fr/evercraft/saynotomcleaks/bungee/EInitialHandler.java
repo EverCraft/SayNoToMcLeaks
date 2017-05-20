f73661dfc29c50e37d5b7c427858922b225a2209
rexbut <butbutdu45@gmail.com>
2017-05-20 13:53:26 +0200

    Fix : https://pastebin.com/xGX2Ukc6

--- src/main/java/fr/evercraft/saynotomcleaks/bungee/EInitialHandler.java
+++ src/main/java/fr/evercraft/saynotomcleaks/bungee/EInitialHandler.java
@@ -40,7 +40,7 @@ public class EInitialHandler extends InitialHandler {
 			} else {
 				this.plugin.debug("The player " + name + " is present in cache (alt account).");
 			}
-			this.plugin.getListener().send(name, false);
+			this.plugin.getListener().send(name, value);
 			return;
 		}
 
